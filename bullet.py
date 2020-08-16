b8152c707ad82cd8594a60e90e92fb81e7dd7761
waqasbinhamed <53257755+waqasbinhamed@users.noreply.github.com>
2020-08-16 18:13:55 +0500

    first commit

---
+++ bullet.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self, ai_setting, screen, ship):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = screen
+
+        # Create a bullet rect at (0, 0) and then set the correct position.
+        self.rect = pygame.Rect(0, 0, ai_setting.bullet_width, ai_setting.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store the bullet's position as a decimal value.
+        self.y = float(self.rect.y)
+
+        self.color = ai_setting.bullet_color
+        self.speed_factor = ai_setting.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.speed_factor
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
